da8c771bb39098d9cffc64a8e5edffac7ebd1f85
sunnymansuri <124335768+sunnymansuri@users.noreply.github.com>
2025-02-06 15:12:40 +0530

    Update main.tf

--- ep1/main.tf
+++ ep1/main.tf
@@ -2,6 +2,6 @@
 
 resource "google_storage_bucket" "bucket" {
   project  = "daring-keep-446309-u4"
-  name     = "daslearning-bucket-23"
+  name     = "daslearning-bucket-2434323"
   location = "ASIA-SOUTH1"
 }
